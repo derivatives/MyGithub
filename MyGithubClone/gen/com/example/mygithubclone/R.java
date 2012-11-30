7cf959f7dae67126424ce67dd373e94b04bd8a36
AquaSoft <AquaSoft@aquasoft-pc>
2012-11-30 09:09:18 +0700

    Add hello nu

--- MyGithubClone/gen/com/example/mygithubclone/R.java
+++ MyGithubClone/gen/com/example/mygithubclone/R.java
@@ -14,8 +14,9 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070001;
+        public static final int menu_settings=0x7f070002;
         public static final int textView1=0x7f070000;
+        public static final int textView2=0x7f070001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -27,6 +28,7 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int hello_aquasoft=0x7f040002;
         public static final int hello_mo=0x7f040005;
+        public static final int hello_nu=0x7f040007;
         public static final int hello_opor=0x7f040003;
         public static final int hello_toomtarm=0x7f040006;
         public static final int hello_world=0x7f040001;
