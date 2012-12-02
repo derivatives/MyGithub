134852fb9ac3151a5b9cdb21f3c73a2142ff9df7
Ice5050 <ice50505050@hotmail.com>
2012-12-02 15:58:58 +0700

    Hello Ice5050
    Change-Id: I82bda45bc1eeb834d4957f15dc46dccba8b77cc8

--- MyGithubClone/gen/com/example/mygithubclone/R.java
+++ MyGithubClone/gen/com/example/mygithubclone/R.java
@@ -14,12 +14,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070005;
+        public static final int menu_settings=0x7f070006;
         public static final int textView1=0x7f070000;
         public static final int textView2=0x7f070001;
         public static final int textView3=0x7f070002;
         public static final int textView4=0x7f070003;
         public static final int textView5=0x7f070004;
+        public static final int textView6=0x7f070005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -31,6 +32,7 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int hello_ae=0x7f040008;
         public static final int hello_aquasoft=0x7f040002;
+        public static final int hello_ice5050=0x7f040009;
         public static final int hello_mo=0x7f040005;
         public static final int hello_nu=0x7f040007;
         public static final int hello_opor=0x7f040003;
